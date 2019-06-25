1cdada7cf412cf5309c6d3575bc1807078ed6f96
yaozhihan1994 <yaozhihan1994@163.com>
2019-06-25 12:34:24 +0800

    modify bild.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,16 @@
 #! /bin/bash
+read -t 5 -p "请输入编译类型（ARM输入 1；X86输入 2；X64输入 3）: " default_Compiler_environment
 
-ARM="ARM"
-X86="X86"
-X64="X64"
-
-default_Compiler_environment=$X64
-
-if [ $default_Compiler_environment == $ARM ]
+if [ $default_Compiler_environment == 1 ]
 then 
 	echo "Compiler_environment : ARM"
 	source /opt/fsl-imx-fb/4.9.88-2.0.0/environment-setup-cortexa9hf-neon-poky-linux-gnueabi
 	cp ./lib/libarm/* ./lib	
-elif [ $default_Compiler_environment == $X86 ]
+elif [ $default_Compiler_environment == 2 ]
 then
 	echo "Compiler_environment : X86"
 	cp ./lib/lib32/* ./lib	
-elif [ $default_Compiler_environment == $X64 ]
+elif [ $default_Compiler_environment == 3 ]
 then
 	echo "Compiler_environment : X64"
 	cp ./lib/lib64/* ./lib	
